a13d9f28da8309fa6ba48df578c6fa6087e42f53
cecvic <40574945+cecvic@users.noreply.github.com>
2025-01-13 23:23:26 +0530

    Update mai2n.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_origins=[
         "*"  # All preview deployments
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
